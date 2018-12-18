17cb0720f3815be250b625554cf38974989f36de
BilalyC <bilaly.camara@outlook.com>
2018-12-18 14:55:01 +0100

    Exercice 4 Complet

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shopping = new List<string>();
+            shopping.Add("Carottes");
+            shopping.Add("Oignons");
+            shopping.Add("Pommes de terre");
+            shopping.Add("Salade");
+            shopping.Add("Tomate");
+
+            //List<string> shopping = new List<string> {"Carottes", "Oignons", "Pommes de terre", "Salade", Tomate"};
+
+            foreach (string element in shopping)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
